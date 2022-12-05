306eb64e5d005d84e419d6d6b19144679b825bf6
hafsamanzoordar <81043146+hafsamanzoordar@users.noreply.github.com>
2022-12-05 09:16:55 +0500

    minor

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,7 @@ router.get("/:id", userController.user_get_by_id);
 
 router.put("/:id", userController.updateUser);
 
-router.put("/inactive/:id", userController.make_user_inactive);
+router.get("/inactive/:id", userController.make_user_inactive);
 
 router.delete("/:id", userController.deleteUser);
 
